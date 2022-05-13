85de3da23c7e8edb09ecaa932e0cdc5c1c6fcf07
Peeps <jeff@peepsx.com>
2022-05-13 11:23:55 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import "./starfield.css";
+import "./starfield.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
